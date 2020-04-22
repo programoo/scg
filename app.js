d4e0854e4fc0dab3391645627eda11ee4c3e1a08
programoo <prakarnwongsanit@gmail.com>
2020-04-22 15:52:24 +0700

    Can debug inside direction js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express = require('express');
-const https = require('https');
-const http = require('http');
+//const express = require('express');
+//const app = express();
+//app.listen(3000);
 
-const API_KEY = 'AIzaSyCrs4tHo0EfCYFaZ08FHvmNQYVMgF3RqBA';
+const direction = require('./direction');
 
-const app = express();
-app.listen(3000);
+direction.get
 
 console.log("Hello Node This Is my First Branch");
-console.error("Ooops someting went wrong");
 console.dir({name: "Andrew", age: 33});
 
 function printMessage(username, badgeCount, point) {
@@ -43,36 +41,3 @@ A = 21
 A_B = 23
 A_C = -21
 findBC(A, A_B, A_C)
-// Call API
-// centralWorldLocation = 13.7466304,100.5393351
-
-try {
-    // url = `https://maps.gleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCrs4tHo0EfCYFaZ08FHvmNQYVMgF3RqBA`
-    const request = https.get('https://teamtreehouse.com/s.json', response => {
-        if(response.statusCode === 200){
-            let body = "";
-            response.on('data', data => {
-                body += data.toString()
-            });
-            response.on('end', () => {
-                try {
-                    const locations = JSON.parse(body);
-                    console.dir(locations.routes);
-                } catch (error) {
-                    console.error(error.message);
-                }
-            });
-        }
-        else{
-            const message = `There was an error getting the profile`
-            const statusCodeError = new Error(message);
-            console.error(`${statusCodeError.message} ${response.statusCode}: ${http.STATUS_CODES[response.statusCode]}`);
-
-            //console.error( `There was an error getting the website ${http.STATUS_CODES[response.statusCode]}` )
-        }
-    });
-
-    request.on('error', error => console.error(`Problem with request: ${error.message}`))
-} catch (error) {
-    console.error(error.message);
-}
---
+++ direction.js
@@ -0,0 +1,39 @@
+// centralWorldLocation = 13.7466304,100.5393351
+const https = require('https');
+const http = require('http');
+const API_KEY = 'AIzaSyCrs4tHo0EfCYFaZ08FHvmNQYVMgF3RqBA';
+
+function getDirection() {
+    console.log("Please Say Something");
+    try {
+        const url = `https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCrs4tHo0EfCYFaZ08FHvmNQYVMgF3RqBA`
+        const request = https.get(url, response => {
+            if (response.statusCode === 200) {
+                let body = "";
+                response.on('data', data => {
+                    body += data.toString()
+                });
+                response.on('end', () => {
+                    try {
+                        const locations = JSON.parse(body);
+                        console.dir(locations.routes);
+                        return locations;
+                    } catch (error) {
+                        console.error(`Cannot parse json object: ${error.message}`);
+                    }
+                });
+            } else {
+                const message = `There was an error getting the profile`
+                const statusCodeError = new Error(message);
+                console.error(`${statusCodeError.message} ${response.statusCode}: ${http.STATUS_CODES[response.statusCode]}`);
+            }
+        });
+
+        request.on('error', error => console.error(`Problem with request: ${error.message}`))
+    } catch (error) {
+        console.error(`Exception Error: ${error.message}`);
+    }
+}
+
+module.exports.get = getDirection();
+console.log("What Happen ");
