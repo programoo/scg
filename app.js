99c87c96467a238a8b215c650df88c2c65053da5
programoo <prakarnwongsanit@gmail.com>
2020-04-22 12:28:43 +0700

    Get BC

--- app.js
+++ app.js
@@ -25,3 +25,19 @@ function isString(characters, target) {
 
 isString(['X', 'Y', '5', '9', '23', 'Z', "", null, undefined, 0]);
 
+
+function findBC(x, y, z){
+    const B = y - x;
+    const C = z - x;
+    const result = {B: B, C: C}
+
+    console.dir(result)
+
+    return result;
+}
+
+
+A = 21
+A_B = 23
+A_C = -21
+findBC(A, A_B, A_C)
