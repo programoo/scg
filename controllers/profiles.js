cf40253587f5f1dd6b5b13f21f15839d0f07dbe5
programoo <prakarnwongsanit@gmail.com>
2020-04-26 18:30:15 +0700

    Add async handler

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+    return async (req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    }
+}
+
+router.get('/', asyncHandler(async (req, res) => {
     console.log(req.query);
     res.render('profiles/index');
-})
+}))
 
 module.exports = router;
