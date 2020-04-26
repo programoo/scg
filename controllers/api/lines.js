5e896f89f2f7153b93bd7866b5e51cfba74ee46a
programoo <prakarnwongsanit@gmail.com>
2020-04-26 19:55:01 +0700

    Clean up line API

--- controllers/api/lines.js
+++ controllers/api/lines.js
@@ -2,31 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const request = require('request');
-const channel_excess_token = 'gKhGxHBRkyngeOe337T2dGxAeTpzAdF1N0xyHxRnJB6RIm9ZcTbsOiweAnzQQWiNeikIToTnasPc60IQu5tpxPNvnIweF5wMCYjCoEoWyWwrXowmVOAYx/l5BN4/NaEO0u43MMQw29F9lpkSG0NljgdB04t89/1O/w1cDnyilFU='
+const CHANNEL_ACCESS_TOKEN = 'gKhGxHBRkyngeOe337T2dGxAeTpzAdF1N0xyHxRnJB6RIm9ZcTbsOiweAnzQQWiNeikIToTnasPc60IQu5tpxPNvnIweF5wMCYjCoEoWyWwrXowmVOAYx/l5BN4/NaEO0u43MMQw29F9lpkSG0NljgdB04t89/1O/w1cDnyilFU='
 
 router.get('/webhook', (req, res) => {
-    console.log("I saw line message here on GET");
     res.send("<h1>Hello Line Messaging Developer</h1>");
 })
 
 router.post('/webhook', (req, res) => {
-    console.log("Hi");
-    let reply_token = req.body.events[0].replyToken
+    let replyToken = req.body.events[0].replyToken
     let userId = req.body.events[0].source.userId;
 
-    console.log(`UserId ${userId}`);
-
     try {
-
         let msg = req.body.events[0].message.text
 
         if (msg.toLowerCase() === "hello".toLowerCase()) {
             const message = "Hi, I am an innocent robot. Nice to meet you."
-            console.log(message);
-            reply(reply_token, message);
+            reply(replyToken, message);
         } else if ((msg.toLowerCase() === "Good Bye".toLowerCase())) {
             const message = "Take care of yourself.";
-            reply(reply_token, message);
+            reply(replyToken, message);
         } else {
             sendError(userId);
             res.sendStatus(200);
@@ -37,7 +31,6 @@ router.post('/webhook', (req, res) => {
         res.sendStatus(200)
     }
 })
-
 
 function sendError(userId) {
     const errorMessage = "It seems our bot cannot answer your question. Could you please try sending Hello or Good bye instead?"
@@ -52,7 +45,7 @@ function sendError(userId) {
 function pushMessage(userId, msg) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${channel_excess_token}`
+        'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`
     }
 
     let body = JSON.stringify({
@@ -72,14 +65,14 @@ function pushMessage(userId, msg) {
     });
 }
 
-function reply(reply_token, msg) {
+function reply(replyToken, msg) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${channel_excess_token}`
+        'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`
     }
 
     let body = JSON.stringify({
-        replyToken: reply_token,
+        replyToken: replyToken,
         messages: [{
             type: 'text',
             text: msg
